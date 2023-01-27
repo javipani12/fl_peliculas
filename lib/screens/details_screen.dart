1379009db043263ae6933546667079af5dd9d745
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2023-01-27 10:21:38 +0100

    clase 27/1/23

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,9 +6,141 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                width: double.infinity,
+                height: 300,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: const FadeInImage(
+                                fit: BoxFit.cover,
+                                width: 150,
+                                height: 190,
+                                placeholder: AssetImage('assets/loading.gif'),
+                                image: NetworkImage('https://es.web.img3.acsta.net/pictures/14/05/05/12/46/010208.jpg'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            width: double.infinity,
+                            height: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Título: Al Filo del Mañana',
+                                  style: Theme.of(context).textTheme.headline5,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  textAlign: TextAlign.left,
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  '2014',
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                  textAlign: TextAlign.left,
+                                ),
+                                Row(
+                                  children: [
+                                    Text('Puntuación: '),
+                                    FadeInImage(
+                                      width: 25,
+                                      height: 25,
+                                      placeholder: AssetImage('assets/loading.gif'), 
+                                      image: AssetImage('assets/pngwing.com.png'),
+                                    ),
+                                    FadeInImage(
+                                      width: 25,
+                                      height: 25,
+                                      placeholder: AssetImage('assets/loading.gif'), 
+                                      image: AssetImage('assets/pngwing.com.png'),
+                                    ),
+                                    FadeInImage(
+                                      width: 25,
+                                      height: 25,
+                                      placeholder: AssetImage('assets/loading.gif'), 
+                                      image: AssetImage('assets/pngwing.com.png'),
+                                    ),
+                                    FadeInImage(
+                                      width: 25,
+                                      height: 25,
+                                      placeholder: AssetImage('assets/loading.gif'), 
+                                      image: AssetImage('assets/pngwing.com.png'),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Sinopsis',
+                            style: Theme.of(context).textTheme.headline5,  
+                          )
+                        ],
+                      ),
+                    )
+                  ]
+                ),
+              )
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      snap: false,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black38,
+          child: Text(
+            'Al Filo del Mañana',
+            style: TextStyle(
+              fontSize: 16,
+
+            ),
+          )
+        ),
+        background: FadeInImage(
+          fit: BoxFit.cover,
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://i.blogs.es/12cece/al-filo-del-manana/1366_2000.jpeg')
         ),
       ),
     );
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,14 +7,13 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 350,
-      color: Colors.red,
+      height: 290,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: const Text('Favoritas', style: TextStyle(
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text('Favoritas', style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
               )
@@ -26,15 +25,49 @@ class MovieSlider extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: 20,
               itemBuilder: (context, index) {
-                return Container(
-                  width: 130,
-                  height: 190,
-                  color: Colors.deepPurple,
-                  margin: const EdgeInsets.all(10),
-                );
+                return _MoviePoster();
               },
             ),
           )
+        ],
+      ),
+    );
+  }
+}
+
+// _ Elemento privado
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+            // Póster
+            GestureDetector(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/loading.gif'), 
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              onTap: () => Navigator.pushNamed(context, 'details'),
+            ),
+            SizedBox(height: 10,),
+            
+            // Título
+            Text('El Señor de los Anillos: El Retorno del Rey', 
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
         ],
       ),
     );
